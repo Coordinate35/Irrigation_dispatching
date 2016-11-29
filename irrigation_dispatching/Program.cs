158566a296ce85eed8c198566751547a63cdeb85
Coordinate35 <Coordinate35@bingyan.net>
2016-11-29 19:42:29 +0800

    echo helloworld

--- irrigation_dispatching/Program.cs
+++ irrigation_dispatching/Program.cs
@@ -17,6 +17,7 @@ namespace irrigation_dispatching
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.WriteLine("hello world");
         }
     }
 }
