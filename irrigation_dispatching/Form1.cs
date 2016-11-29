03c338a39c85fd186b70f413e85e5e45de907a12
Coordinate35 <Coordinate35@bingyan.net>
2016-11-29 19:35:59 +0800

    directory structure

--- irrigation_dispatching/Form1.cs
+++ irrigation_dispatching/Form1.cs
@@ -16,5 +16,10 @@ namespace irrigation_dispatching
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
