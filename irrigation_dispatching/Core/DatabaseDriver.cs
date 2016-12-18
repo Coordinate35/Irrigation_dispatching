1c7d7eda333d28441112a6c49826e297c97c8261
Coordinate35 <Coordinate35@bingyan.net>
2016-12-18 18:01:23 +0800

    database driver get data

--- irrigation_dispatching/Core/DatabaseDriver.cs
+++ irrigation_dispatching/Core/DatabaseDriver.cs
@@ -16,7 +16,13 @@ namespace irrigation_dispatching.Core
         private string pwd;
         private string connectString;
         private SqlConnection connection;
-        private string lastError;
+        private Dictionary<string, string> partialCommand;
+        private string[] commandList;
+        private Dictionary<string, bool> commandsInCommandGet;
+        private string error;
+        private string lastQuery;
+        private SqlCommand command;
+        private SqlDataReader resultReader;
 
         public DatabaseDriver(string dataSource, string initialCatalog, string userId, string pwd, string persistSecurityInfo = "True")
         {
@@ -25,10 +31,36 @@ namespace irrigation_dispatching.Core
             this.userId = userId;
             this.pwd = pwd;
             this.persistSecurityInfo = persistSecurityInfo;
-            generateConnectString();
+            GenerateConnectString();
+
+            partialCommand = InitializePartialCommand();
+            commandsInCommandGet = InitializeGetCommandList();
         }
 
-        private void generateConnectString()
+        private Dictionary<string, string> InitializePartialCommand()
+        {
+            Dictionary<string, string> commands = new Dictionary<string, string>();
+            commandList = new string[] { "select", "from", "where", "orderBy", "limit" };
+            foreach (string commandName in commandList)
+            {
+                commands.Add(commandName, null);
+            }
+
+            return commands;
+        }
+
+        private Dictionary<string, bool> InitializeGetCommandList()
+        {
+            Dictionary<string, bool> commandList = new Dictionary<string, bool>();
+            commandList.Add("select", true);
+            commandList.Add("from", true);
+            commandList.Add("where", false);
+            commandList.Add("orderBy", false);
+            commandList.Add("limit", false);
+            return commandList;
+        }
+
+        private void GenerateConnectString()
         {
             connectString = String.Format(
                 "Data Source={0};Initial Catalog={1};Persist Security Info={2};User ID={3};Pwd={4}",
@@ -40,7 +72,7 @@ namespace irrigation_dispatching.Core
             );
         }
 
-        public bool connect()
+        public bool Connect()
         {
             try
             {
@@ -49,7 +81,91 @@ namespace irrigation_dispatching.Core
             }
             catch (SqlException e)
             {
-                lastError = e.ToString();
+                error = e.ToString();
+                return false;
+            }
+            return true;
+        }
+
+        public void SetSelect(string columns)
+        {
+            partialCommand["select"] = "SELECT " + columns;
+        }
+
+        public void SetFrom(string tables)
+        {
+            partialCommand["from"] = "FROM " + tables;
+        }
+
+        public void SetAndWhere(string condition)
+        {
+            partialCommand["where"] = "WHERE " + condition;
+        }
+
+        public void SetAndWhere(string key, string value)
+        {
+            if (null == partialCommand["where"])
+            {
+                SetFirstWhere(key, value);
+            }
+            partialCommand["where"] = partialCommand["where"] + " " + key + "=" + value; 
+        }
+
+        public void SetAndWhere(string key, int value)
+        {
+            string stringValue = value.ToString();
+            SetAndWhere(key, stringValue);
+        }
+
+        private void SetFirstWhere(string key, string value)
+        {
+            partialCommand["where"] = "WHERE " + key + "=" + value;
+        }
+
+        public void SetOrderBy(string colume, string direction = "ASC")
+        {
+            partialCommand["orderBy"] = "ORDER BY " + colume + " " + direction;
+        }
+
+        public void SetLimit(int limitNumber, int offset)
+        {
+            partialCommand["limit"] = "LIMIT " + limitNumber.ToString() + ", " + offset.ToString();
+        }
+
+        private bool GenerateGetQuery()
+        {
+            foreach (KeyValuePair<string, bool> singleCommand in commandsInCommandGet)
+            {
+                if ((true == singleCommand.Value) && (null == partialCommand[singleCommand.Key]))
+                {
+                    return false;
+                }
+                if (null == lastQuery)
+                {
+                    lastQuery = partialCommand[singleCommand.Key];
+                }
+                else
+                {
+                    lastQuery += partialCommand[singleCommand.Key];
+                }
+            }
+            return true;
+        }
+
+        public bool Get()
+        {
+            if (false == GenerateGetQuery())
+            {
+                return false;
+            }
+            try
+            {
+                command = new SqlCommand(lastQuery, connection);
+                resultReader = command.ExecuteReader();
+            }
+            catch (Exception e)
+            {
+                error = e.ToString();
                 return false;
             }
             return true;
