02570d80d047b7d6ee6fa6bcb05a1c55067022a0
Coordinate35 <Coordinate35@bingyan.net>
2016-12-28 17:47:53 +0800

    import data sql

---
+++ irrigation_dispatching/maintain/database/dry_earth.sql
@@ -0,0 +1,15 @@
+﻿USE irrigation_dispatching;
+GO
+
+INSERT INTO dry_earth(round_order, area) 
+VALUES
+(1,	65488),
+(2,	0),
+(3,	0),
+(4,	0),
+(5,	0),
+(6,	0),
+(7,	0),
+(8,	0),
+(9,	344209);
+GO
---
+++ irrigation_dispatching/maintain/database/irrigation_method.sql
@@ -0,0 +1,11 @@
+﻿USE irrigation_dispatching;
+GO
+
+INSERT INTO irrigation_method(crop_id,method ,designed_output_min,disigned_output_max ) 
+VALUES 
+(1,	'块灌',	300,450),
+(3,	'块灌',	350,500),
+(5,	'块灌',	90,145),
+(13,	'块灌',	100,200),
+(14,	'块灌',	100,200);
+GO
